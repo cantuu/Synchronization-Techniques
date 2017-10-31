53b285e025355046be5792b5693167c29e1389f9
cantuu <gabrielcantu11@gmail.com>
2017-10-30 23:58:43 -0200

    Mueller & Muller Already Works in API

--- src/SymbolSynchronizer.m
+++ src/SymbolSynchronizer.m
@@ -6,7 +6,7 @@ classdef SymbolSynchronizer
     % Gardner (Normal ou Decided) ou Mueller & Muller
     % O default do Matlab eh o Zero-Crossing, porem aqui sera o Mueller & Muller
     % (Por enquanto... Zero-Crossing sera implementado)
-    TimingErrorDetector = 'Mueller-Muller';
+    TimingErrorDetector = 'Mueller & Muller';
     
     % Especifica o numero de amostras por simbolo, sendo um numero real, inteiro,
     % escalar e maior igual a 2. O default do Matlab eh 2.
@@ -27,8 +27,8 @@ classdef SymbolSynchronizer
         'Early-Late'; 'Early-Late Decided'; 'Early-Late NDA'; ...
         'Gardner'; 'Gardner Decided'; 'Mueller & Muller'];
   end
-
   
+
   methods
     % Construtor e inicializaçao de variaveis
     % ATENÇAO! REALIZAR A VERIFICAÇAO DAS VARIAVEIS (ver se bate com os requisitos...)
@@ -48,17 +48,61 @@ classdef SymbolSynchronizer
       end  
     end
   
-    function s = step(obj, x)
-      printf("%d\n", x)
-      
+    function s = step(obj, y)
+      #if obj.TimingErrorDetector == 'Mueller & Muller'
+      #  s = obj.mueller_muller(y);
+      #end
+      if obj.TimingErrorDetector == 'Gardner'
+        s = obj.gardner(y);
+      end  
     end 
-  
+        
+    function instants = mueller_muller(obj, y)
+      k=0; tau_hat=0; instants=[];
+      i = obj.SamplesPerSymbol + 1;
+      
+      bw = obj.NormalizedLoopBandwidth;
+      damp = obj.DampingFactor
+      beta = (4*bw*bw)/(1 + 2*damp*bw + bw*bw)
+      alpha = (4*damp*bw)/(1 + 2*damp*bw + bw*bw)
+      
+      while k <= length(y) - 2*obj.SamplesPerSymbol
+        k = round(i+tau_hat);
+        k1 = round(i+tau_hat-obj.SamplesPerSymbol);
+        e = (sign(y(k1))*y(k)) - (sign(y(k))*y(k1));
+        obj.SamplesPerSymbol += e*beta;
+        tau_hat += beta + e*alpha;
+        instants = [instants k];
+        i += obj.SamplesPerSymbol;
+      end
+    end
+    
+    function instants = gardner(obj, y)
+      k=0; tau_hat=0; instants=[]; 
+      tau_hat1 = 0;
+      i = obj.SamplesPerSymbol + 1;
+      
+      bw = obj.NormalizedLoopBandwidth;
+      damp = obj.DampingFactor;
+      beta = (4*bw*bw)/(1 + 2*damp*bw + bw*bw);
+      alpha = (4*damp*bw)/(1 + 2*damp*bw + bw*bw);
+      
+      while k <= length(y) - 2*obj.SamplesPerSymbol
+        k = round(i + tau_hat);
+        k1 = round(i + tau_hat1 - obj.SamplesPerSymbol);
+        k_half = ((k1 + k)/2);
+        e = (y(k1) - y(k)) * y(round(k_half));
+        obj.SamplesPerSymbol += e*beta;
+        tau_hat1 = tau_hat;
+        tau_hat += beta + e*alpha;
+        instants = [instants k];
+        i += obj.SamplesPerSymbol;
+      end
+    end    
+
+    
   end % methods
   
 end %class
 
 % [EOF]
-
-% Exemplo de Construtor
-% a = SymbolSynchronizer('TimingErrorDetector', 'Early-Late', 'SamplesPerSymbol', ...
-%     5, 'DampingFactor', 0.5, 'NormalizedLoopBandwidth', 0.005, 'DetectorGain', 2)
